94350394a67e922577b514ebbdfc3d866f4cb7a5
tako <takosama.312@gmail.com>
2018-05-05 21:14:12 +0900

    multi task 対応

--- ConsoleApp67/Program.cs
+++ ConsoleApp67/Program.cs
@@ -35,9 +35,9 @@ namespace ConsoleApp67
 
                     img[i] = (byte)i;
                 }
-                var pprt = ptr;
+                var ppprt = ptr;
 
-                var pp = p;
+                var ppp = p;
                 int h = 1080;
                 int w = 1920;
                 Vector128<float> r = Sse.SetVector128(.333f, .333f, .333f, .333f);
@@ -48,32 +48,34 @@ namespace ConsoleApp67
                 Vector128<sbyte> maskb = Sse2.SetVector128(-1, -1, -1, 14, -1, -1, -1, 10, -1, -1, -1, 6, -1, -1, -1, 2);
                 Vector128<sbyte> maskrtn = Sse2.SetVector128(-1, 12, 12, 12, -1, 8, 8, 8, -1, 4, 4, 4, -1, 0, 0, 0);
 
-                for (int y = 0; y < h; y++)
-                {
-                    for (int x = 0; x < w; x += 4)
-                    {
-                        var tmp0 = Sse.StaticCast<byte, sbyte>(Sse2.LoadVector128(pprt));
+                Parallel.For(0, h, y =>
+                   {
+                       var pprt = ppprt + 4 * y + w;
+                       var pp = ppp + 4 * y + w;
+                       for (int x = 0; x < w; x += 4)
+                       {
+                           var tmp0 = Sse.StaticCast<byte, sbyte>(Sse2.LoadVector128(pprt));
 
-                        var t0 = Ssse3.Shuffle(tmp0, maskr);
-                        var t1 = Ssse3.Shuffle(tmp0, maskg);
-                        var t2 = Ssse3.Shuffle(tmp0, maskb);
+                           var t0 = Ssse3.Shuffle(tmp0, maskr);
+                           var t1 = Ssse3.Shuffle(tmp0, maskg);
+                           var t2 = Ssse3.Shuffle(tmp0, maskb);
 
-                        var tmp6 = Sse2.ConvertToVector128Single(Sse.StaticCast<sbyte, int>(t0));
-                        var tmp7 = Sse2.ConvertToVector128Single(Sse.StaticCast<sbyte, int>(t1));
-                        var tmp8 = Sse2.ConvertToVector128Single(Sse.StaticCast<sbyte, int>(t2));
+                           var tmp6 = Sse2.ConvertToVector128Single(Sse.StaticCast<sbyte, int>(t0));
+                           var tmp7 = Sse2.ConvertToVector128Single(Sse.StaticCast<sbyte, int>(t1));
+                           var tmp8 = Sse2.ConvertToVector128Single(Sse.StaticCast<sbyte, int>(t2));
 
 
-                        var tmp13 = Sse.Add(Sse.Add(Sse.Multiply(tmp6, r), Sse.Multiply(tmp7, g)), Sse.Multiply(tmp8, b));
+                           var tmp13 = Sse.Add(Sse.Add(Sse.Multiply(tmp6, r), Sse.Multiply(tmp7, g)), Sse.Multiply(tmp8, b));
 
-                        var tmp14 = Sse.StaticCast<int, sbyte>(Sse2.ConvertToVector128Int32(tmp13));
+                           var tmp14 = Sse.StaticCast<int, sbyte>(Sse2.ConvertToVector128Int32(tmp13));
 
-                        var tmp18 = Ssse3.Shuffle(tmp14, maskrtn);
+                           var tmp18 = Ssse3.Shuffle(tmp14, maskrtn);
 
-                        Sse2.Store(pp, Sse.StaticCast<sbyte, byte>(tmp18));
-                        pp += 16;
-                        pprt += 16;
-                    }
-                }
+                           Sse2.Store(pp, Sse.StaticCast<sbyte, byte>(tmp18));
+                           pp += 16;
+                           pprt += 16;
+                       }
+                   });
             }
         }
     }
