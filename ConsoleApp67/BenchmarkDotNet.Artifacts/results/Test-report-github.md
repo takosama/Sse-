d5f3c2d3c8b654888b367e108ae97ce99f8bba3f
tako <takosama.312@gmail.com>
2018-05-05 21:06:37 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp67/BenchmarkDotNet.Artifacts/results/Test-report-github.md
@@ -0,0 +1,14 @@
+``` ini
+
+BenchmarkDotNet=v0.10.14, OS=Windows 10.0.16299.371 (1709/FallCreatorsUpdate/Redstone3)
+Intel Core i7-4710MQ CPU 2.50GHz (Haswell), 1 CPU, 8 logical and 4 physical cores
+Frequency=2435767 Hz, Resolution=410.5483 ns, Timer=TSC
+.NET Core SDK=2.2.100-preview1-008633
+  [Host]     : .NET Core 2.1.0-preview3-26411-06 (CoreCLR 4.6.26411.07, CoreFX 4.6.26411.06), 64bit RyuJIT
+  DefaultJob : .NET Core 2.1.0-preview3-26411-06 (CoreCLR 4.6.26411.07, CoreFX 4.6.26411.06), 64bit RyuJIT
+
+
+```
+| Method |     Mean |     Error |    StdDev |
+|------- |---------:|----------:|----------:|
+|    Run | 7.192 ms | 0.1102 ms | 0.1082 ms |
---
+++ ConsoleApp67/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using System.Runtime.Intrinsics.X86;
+using System.Runtime.Intrinsics;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp67
+
+{
+    class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            test.Run();
+            BenchmarkDotNet.Running.BenchmarkRunner.Run<test>();
+        }
+    }
+
+    public class test
+    {
+        [Benchmark]
+        unsafe public static void Run()
+        {
+            byte[] img = new byte[1920 * 1080 * 4];
+            byte[] canvus = new byte[1920 * 1080 * 4];
+
+            fixed (byte* ptr = &img[0])
+            fixed (byte* p = &canvus[0])
+            {
+                //ばぐとりようの数字
+                for (int i = 0; i < 255; i++)
+                {
+
+                    img[i] = (byte)i;
+                }
+                var pprt = ptr;
+
+                var pp = p;
+                int h = 1080;
+                int w = 1920;
+                Vector128<float> r = Sse.SetVector128(.333f, .333f, .333f, .333f);
+                Vector128<float> g = Sse.SetVector128(.666f, .666f, .666f, .666f);
+                Vector128<float> b = Sse.SetVector128(.112f, .112f, .112f, .112f);
+                Vector128<sbyte> maskr = Sse2.SetVector128(-1, -1, -1, 12, -1, -1, -1, 8, -1, -1, -1, 4, -1, -1, -1, 0);
+                Vector128<sbyte> maskg = Sse2.SetVector128(-1, -1, -1, 13, -1, -1, -1, 9, -1, -1, -1, 5, -1, -1, -1, 1);
+                Vector128<sbyte> maskb = Sse2.SetVector128(-1, -1, -1, 14, -1, -1, -1, 10, -1, -1, -1, 6, -1, -1, -1, 2);
+                Vector128<sbyte> maskrtn = Sse2.SetVector128(-1, 12, 12, 12, -1, 8, 8, 8, -1, 4, 4, 4, -1, 0, 0, 0);
+
+                for (int y = 0; y < h; y++)
+                {
+                    for (int x = 0; x < w; x += 4)
+                    {
+                        var tmp0 = Sse.StaticCast<byte, sbyte>(Sse2.LoadVector128(pprt));
+
+                        var t0 = Ssse3.Shuffle(tmp0, maskr);
+                        var t1 = Ssse3.Shuffle(tmp0, maskg);
+                        var t2 = Ssse3.Shuffle(tmp0, maskb);
+
+                        var tmp6 = Sse2.ConvertToVector128Single(Sse.StaticCast<sbyte, int>(t0));
+                        var tmp7 = Sse2.ConvertToVector128Single(Sse.StaticCast<sbyte, int>(t1));
+                        var tmp8 = Sse2.ConvertToVector128Single(Sse.StaticCast<sbyte, int>(t2));
+
+
+                        var tmp13 = Sse.Add(Sse.Add(Sse.Multiply(tmp6, r), Sse.Multiply(tmp7, g)), Sse.Multiply(tmp8, b));
+
+                        var tmp14 = Sse.StaticCast<int, sbyte>(Sse2.ConvertToVector128Int32(tmp13));
+
+                        var tmp18 = Ssse3.Shuffle(tmp14, maskrtn);
+
+                        Sse2.Store(pp, Sse.StaticCast<sbyte, byte>(tmp18));
+                        pp += 16;
+                        pprt += 16;
+                    }
+                }
+            }
+        }
+    }
+}
